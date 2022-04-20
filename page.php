cc98628b9d11d67e090cd3c917b967a22898476c
Youngjae Lim <limyoungjae@gmail.com>
2022-04-20 15:41:13 -0400

    Added a page-banner and metabox to a blog post template(single.php)

--- page.php
+++ page.php
@@ -15,22 +15,20 @@
     </div>
 
     <div class="container container--narrow page-section">
-        <div>
-            <?php
-                // If the current page has a parent, $theParent will be a parent id.
-                // Otherwise, it will be zero(false).
-                $theParent = wp_get_post_parent_id(get_the_ID());
+        <?php
+            // If the current page has a parent, $theParent will be a parent id.
+            // Otherwise, it will be zero(false).
+            $theParent = wp_get_post_parent_id(get_the_ID());
 
-                // Show breadcrumb for children pages only
-                if ($theParent) { ?>
-                    <div class="metabox metabox--position-up metabox--with-home-link">
-                        <p>
-                        <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
-                        </p>
-                    </div>
-               <?php }
-            ?>
-        </div>
+            // Show breadcrumb for children pages only
+            if ($theParent) { ?>
+                <div class="metabox metabox--position-up metabox--with-home-link">
+                    <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+                    </p>
+                </div>
+            <?php }
+        ?>
 
 
     <?php
