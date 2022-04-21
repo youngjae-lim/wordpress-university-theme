e232043f62b4afe600f77ec3bf119c9b729be0bc
Youngjae Lim <limyoungjae@gmail.com>
2022-04-21 10:56:22 -0400

    Improved excerpt shown in the homepage

--- front-page.php
+++ front-page.php
@@ -30,13 +30,13 @@
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                  <p><?php echo wp_trim_words(get_the_excerpt(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
                 </div>
               </div>
               <?php } wp_reset_postdata();
             ?>
 
-          <p class="t-center no-margin"><a href="<?php echo site_url('/event'); ?>" class="btn btn--blue">View All Events</a></p>
+          <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
       </div>
       <div class="full-width-split__two">
@@ -56,7 +56,7 @@
                   </a>
                   <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                    <p><?php echo wp_trim_words(get_the_excerpt(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
                 </div>
         </div>
               <?php } wp_reset_postdata();
