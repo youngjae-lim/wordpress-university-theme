263dcfa95302a11eb3487dc0cf32ac0322c72dc4
Youngjae Lim <limyoungjae@gmail.com>
2022-04-21 19:40:45 -0400

    Added a single program post and archive program

--- single-program.php
+++ single-program.php
@@ -17,7 +17,7 @@
         <div class="container container--narrow page-section">
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
-                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Events</a> <span class="metabox__main"><?php the_title(); ?></span>
+                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
                 </p>
             </div>
 
