8263345488f2c32e9b001d02ea2a4baf19f86f68
Youngjae Lim <limyoungjae@gmail.com>
2022-04-22 17:41:24 -0400

    Refactored page-banner section to avoid code duplications

--- page.php
+++ page.php
@@ -2,17 +2,10 @@
     get_header();
 
     while(have_posts()) {
-        the_post(); ?>
+        the_post();
+        pageBanner();
+        ?>
 
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Placeholder for Subtitle</p>
-        </div>
-      </div>
-    </div>
 
     <div class="container container--narrow page-section">
         <?php
@@ -39,7 +32,11 @@
     // Show page links for any pages with parent-child relationship only.
     if ($theParent or $hasChild) { ?>
       <div class="page-links">
-        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+        <h2 class="page-links__title">
+            <a href="<?php echo get_permalink($theParent); ?>">
+                <?php echo get_the_title($theParent); ?>
+            </a>
+        </h2>
         <ul class="min-list">
             <?php
                 if ($theParent) { // if the current page is a child
