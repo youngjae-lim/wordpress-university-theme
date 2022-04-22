605c4113b74f8ae00ee9d0e3ad5f59843000beef
Youngjae Lim <limyoungjae@gmail.com>
2022-04-22 01:36:11 -0400

    Used a professorPortrait image for single-professor

--- single-program.php
+++ single-program.php
@@ -42,14 +42,14 @@
 
                 if ($relatedProfessors->have_posts()) {
                     echo '<hr class="section-break">';
-                    echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professor(s)</h2>';
+                    echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
 
                     echo '<ul class="professor-cards">';
                     while($relatedProfessors->have_posts()) {
                         $relatedProfessors->the_post(); ?>
                         <li class="professor-card__list-item">
                             <a class="professor-card" href="<?php the_permalink(); ?>">
-                               <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>" alt="">
+                               <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>" alt="">
                                <span class="professor-card__name">
                                    <?php the_title(); ?>
                                </span>
