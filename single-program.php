73d1bacfb1b652470ef4fe43983b64096b6bd289
Youngjae Lim <limyoungjae@gmail.com>
2022-04-21 22:43:47 -0400

    Added a professor post type and a single professor

--- single-program.php
+++ single-program.php
@@ -23,6 +23,35 @@
 
             <div class="generic-content"><?php the_content(); ?></div>
 
+            <!-- Related Professors -->
+            <?php
+                $today = date('Ymd');
+                $relatedProfessors = new WP_Query(array(
+                    'posts_per_page' => -1, // all posts
+                    'post_type' => 'professor',
+                    'orderby' => 'title',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'related_programs',
+                            'compare' => 'LIKE',
+                            'value' => '"' . get_the_ID() . '"'
+                        )
+                    )
+                ));
+
+                if ($relatedProfessors->have_posts()) {
+                    echo '<hr class="section-break">';
+                    echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professor(s)</h2>';
+
+                    while($relatedProfessors->have_posts()) {
+                        $relatedProfessors->the_post(); ?>
+                        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                    <?php } wp_reset_postdata();
+                }
+                ?>
+
+            <!-- Upcoming Events -->
             <?php
                 $today = date('Ymd');
                 $upcomingEvents = new WP_Query(array(
