386ab77af4f3174e0497f5c5c8391562e7f07bc9
Youngjae Lim <limyoungjae@gmail.com>
2022-04-24 00:26:51 -0400

    Added related campuses to single-program and added related programs to single-campus

--- single-campus.php
+++ single-campus.php
@@ -10,42 +10,47 @@
         <div class="container container--narrow page-section">
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
-                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
+                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Campuses</a> <span class="metabox__main"><?php the_title(); ?></span>
                 </p>
             </div>
 
             <div class="generic-content"><?php the_content(); ?></div>
 
-            <!-- Related Professors -->
+            <div class="acf-map">
+            <?php $mapLocation = get_field('map_location'); ?>
+                <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
+                    <h3><?php the_title(); ?></h3>
+                    <?php echo $mapLocation['address']; ?>
+                </div>
+            </div>
+
+            <!-- Related Programs -->
             <?php
                 $today = date('Ymd');
-                $relatedProfessors = new WP_Query(array(
+                $relatedPrograms = new WP_Query(array(
                     'posts_per_page' => -1, // all posts
-                    'post_type' => 'professor',
+                    'post_type' => 'program',
                     'orderby' => 'title',
                     'order' => 'ASC',
                     'meta_query' => array(
                         array(
-                            'key' => 'related_programs',
+                            'key' => 'related_campuses',
                             'compare' => 'LIKE',
                             'value' => '"' . get_the_ID() . '"'
                         )
                     )
                 ));
 
-                if ($relatedProfessors->have_posts()) {
+                if ($relatedPrograms->have_posts()) {
                     echo '<hr class="section-break">';
-                    echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+                    echo '<h2 class="headline headline--medium">Programs Available At This Campus</h2>';
 
-                    echo '<ul class="professor-cards">';
-                    while($relatedProfessors->have_posts()) {
-                        $relatedProfessors->the_post(); ?>
-                        <li class="professor-card__list-item">
-                            <a class="professor-card" href="<?php the_permalink(); ?>">
-                               <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>" alt="">
-                               <span class="professor-card__name">
-                                   <?php the_title(); ?>
-                               </span>
+                    echo '<ul class="link-list min-list">';
+                    while($relatedPrograms->have_posts()) {
+                        $relatedPrograms->the_post(); ?>
+                        <li>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_title(); ?>
                             </a>
                         </li>
                     <?php } echo '</ul>';
@@ -85,19 +90,6 @@
                         $upcomingEvents->the_post();
                         get_template_part('template-parts/content-event');
                     } wp_reset_postdata();
-                }
-
-                $relatedCampuses = get_field('related_campuses');
-
-                if ($relatedCampuses) {
-                    echo '<hr class="section-break">';
-                    echo '<h2 class="headline headline--medium">' . get_the_title() . ' is Available At These Campuses:</h2>';
-
-                    echo '<ul class="link-list min-list">';
-                    foreach($relatedCampuses as $campus) {
-                        ?> <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a></li> <?php
-                    }
-                    echo '</ul>';
                 }
                 ?>
         </div>
