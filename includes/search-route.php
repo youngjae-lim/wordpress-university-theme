d9d555de7557a7691808f8e14255afa6ee601efe
Youngjae Lim <limyoungjae@gmail.com>
2022-04-27 22:09:49 -0400

    Structured json data with multiple post type

--- includes/search-route.php
+++ includes/search-route.php
@@ -8,22 +8,60 @@ function university_register_search() {
 }
 
 function university_search_results($data) {
-    $professors = new WP_Query(array(
-        'post_type' => 'professor',
+    $mainQuery = new WP_Query(array(
+        'post_type' => array('post', 'page', 'professor', 'program', 'event', 'campus'),
         's' => sanitize_text_field($data['keyword'])
     ));
 
-    $professorResults = array();
+    $results = array(
+        'generalInfo' => array(),
+        'professors' => array(),
+        'programs' => array(),
+        'events' => array(),
+        'campuses' => array()
+    );
 
-    while($professors->have_posts()) {
-        $professors->the_post();
-        array_push($professorResults, array(
-            'title' => get_the_title(),
-            'permalink' => get_the_permalink()
-        ));
+    while($mainQuery->have_posts()) {
+        $mainQuery->the_post();
+
+        if (get_post_type() == 'post' or get_post_type() == 'page') {
+            array_push($results['generalInfo'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
+        if (get_post_type() == 'professor') {
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
+        if (get_post_type() == 'program') {
+            array_push($results['programs'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
+        if (get_post_type() == 'event') {
+            array_push($results['events'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
+        if (get_post_type() == 'campus') {
+            array_push($results['campuses'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
     }
 
-    return $professorResults;
+    return $results;
 }
 
 add_action('rest_api_init', 'university_register_search');
