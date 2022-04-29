b61914bb17f978372c211781fb67d0a0bcabec96
Youngjae Lim <limyoungjae@gmail.com>
2022-04-29 14:07:40 -0400

    Added a custom login screen

--- functions.php
+++ functions.php
@@ -132,3 +132,26 @@ function noSubsAdminBar() {
 }
 
 add_action('wp_loaded', 'noSubsAdminBar');
+
+
+// Customize login screen
+function ourHeaderUrl() {
+    return esc_url(site_url('/'));
+}
+
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourLoginCSS() {
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginTitle() {
+    return get_bloginfo('name');
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
